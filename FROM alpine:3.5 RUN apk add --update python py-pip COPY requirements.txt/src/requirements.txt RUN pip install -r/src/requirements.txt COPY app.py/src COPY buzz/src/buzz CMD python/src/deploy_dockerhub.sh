841077d930713753e252ad5da2a972f9ff4e9b83
ClaytonTrindade10 <47260225+ClaytonTrindade10@users.noreply.github.com>
2019-02-01 22:00:17 -0200

    Update deploy_dockerhub.sh

--- FROM alpine:3.5 RUN apk add --update python py-pip COPY requirements.txt/src/requirements.txt RUN pip install -r/src/requirements.txt COPY app.py/src COPY buzz/src/buzz CMD python/src/deploy_dockerhub.sh
+++ FROM alpine:3.5 RUN apk add --update python py-pip COPY requirements.txt/src/requirements.txt RUN pip install -r/src/requirements.txt COPY app.py/src COPY buzz/src/buzz CMD python/src/deploy_dockerhub.sh
@@ -2,5 +2,5 @@
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 TAG="$TRAVIS_BUILD_NUMBER"
 
-docker build -f Dockerfile -t devfiap/Clayton:$TAG .
-docker push devfiap/Clayton
+docker build -f Dockerfile -t devfiap/clayton:$TAG .
+docker push devfiap/clayton
